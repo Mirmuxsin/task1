e564f68483e39f1f90f44630ca46ff8fd0e8d6ae
Mirmuxsin <millykhamroev@gmail.com>
2021-10-26 17:08:56 +0500

    added wallet table

--- database/migrations/2021_10_26_112901_create_wallet_table.php
+++ database/migrations/2021_10_26_112901_create_wallet_table.php
@@ -4,7 +4,7 @@ use Illuminate\Database\Migrations\Migration;
 use Illuminate\Database\Schema\Blueprint;
 use Illuminate\Support\Facades\Schema;
 
-class CreateBalanceTable extends Migration
+class CreateWalletTable extends Migration
 {
     /**
      * Run the migrations.
@@ -13,13 +13,14 @@ class CreateBalanceTable extends Migration
      */
     public function up()
     {
-        Schema::create('balance', function (Blueprint $table) {
+        Schema::create('wallet', function (Blueprint $table) {
             $table->id();
             $table->unsignedBigInteger('user_id');
+            $table->integer('balance');
             $table->timestamps();
         });
 
-        Schema::table('balance', function (Blueprint $table) {
+        Schema::table('wallet', function (Blueprint $table) {
             $table->foreign('user_id')->references('id')->on('users')->onDelete('cascade');
         });
     }
